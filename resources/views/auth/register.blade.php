d5edc555ae1cadb3def1804bb0415b4e79db3bd6
ghaan1 <gono0608@gmail.com>
2022-06-22 18:45:42 +0700

    View Login : Revisi 1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.layoutBeranda')
 @section('content')
+<div class="breadcrumb-section breadcrumb-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="breadcrumb-text">
+          <p>WaroenkQu</p>
+						<h1>Register</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
