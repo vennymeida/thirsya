15e3fec8d8429faff86913bbe64cd853974f2800
ghaan1 <gono0608@gmail.com>
2022-09-21 08:24:54 +0700

    Laravel-testing: 3

--- tests/Unit/BarangTest.php
+++ tests/Unit/BarangTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use App\Models\User;
 use App\Models\Barang;
 use App\Models\Role;
+use App\Models\Kategori;
 use App\Models\RoleUser;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -18,7 +19,7 @@ class BarangTest extends TestCase
      *
      * @return void
      */
-use RefreshDatabase;
+//use RefreshDatabase;
     public function test_example()
     {
         $this->assertTrue(true);
@@ -29,29 +30,29 @@ use RefreshDatabase;
         $this->get('/')->assertStatus(200);
     }
 
-    public function test_login(){
-        $user = User::factory()->create([
-            'id' => 1,
-            'username' => 'admin123',
-            'password' => bcrypt('admin123')
-        ]);
+    // public function test_login(){
+    //     $user = User::factory()->create([
+    //         'id' => 1,
+    //         'username' => 'admin123',
+    //         'password' => bcrypt('admin123')
+    //     ]);
 
-        Role::factory()->create([
-            'name' => 'admin'
-        ]);
+    //     Role::factory()->create([
+    //         'name' => 'admin'
+    //     ]);
 
-        RoleUser::factory()->create([
-            'user_id' => 1,
-            'role_id' => 1
-        ]);
+    //     RoleUser::factory()->create([
+    //         'user_id' => 1,
+    //         'role_id' => 1
+    //     ]);
 
-        $response = $this->actingAs($user)
-            ->withSession(['data' => 'session'])
-            ->get('/dashboard');
+    //     $response = $this->actingAs($user)
+    //         ->withSession(['data' => 'session'])
+    //         ->get('/dashboard');
 
-        $response = $this->get('/barang');
-        $response->assertStatus(200);
-    }
+    //     $response = $this->get('/barang');
+    //     $response->assertStatus(200);
+    // }
 
     public function test_render_barang_page()
     {
@@ -119,19 +120,23 @@ use RefreshDatabase;
     public function test_delete_barangs_by_id()
     {
         //setup
-        $barang = Barang::create([
-            'Kategori' => 1,
-            'nama_barang' => 'Ghani',
-            'harga' => 100,
-            'stok' => 1,
-            // 'keterangan' => 'Sembarang',
-            // 'gambar' => 'assets/img.jpg',
-        ]);
-        print_r($barang);
-        //do something
-        $this->followingRedirects()->delete($barang->id);
-        //assert
-        $this->assertDatabaseMissing('barangs', $barang->toArray());
+        // $barang = Barang::create([
+        //     'Kategori' => 1,
+        //     'nama_barang' => 'Ghani',
+        //     'harga' => 100,
+        //     'stok' => 1,
+        //     // 'keterangan' => 'Sembarang',
+        //     // 'gambar' => 'assets/img.jpg',
+        // ]);
+        // print_r($barang);
+        // //do something
+        // $this->followingRedirects()->delete($barang->id);
+        // //assert
+        // $this->assertDatabaseMissing('barangs', $barang->toArray());
+        $barang = Barang::where('nama_barang', 'Test Edit Product')->delete();
+
+        $response = $this->get('/dashboard/products');
+        $response->assertStatus(200);
     }
 
     public function test_render_kategori_page()
@@ -143,18 +148,16 @@ use RefreshDatabase;
 
     public function test_store_kategori()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
-        $response = $this->post('/barang', [
-            'nama' => 'Kertas',
+        Kategori::create([
+            'nama' => 'kertas',
         ]);
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/kategori');
         $response->assertStatus(302);
     }
 
     public function test_store_kategori_invalid_input()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
         $response = $this->post('/kategori', [
             'nama' => '',
         ]);
@@ -162,5 +165,30 @@ use RefreshDatabase;
         $response->assertInvalid([
             'nama' => 'The nama field is required.',
         ]);
+    }
+
+    public function test_edit_data_kategori_page()
+    {
+        $kategori = Kategori::create([
+            'nama' => 'kertasoke',
+        ]);
+
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/kategori/edit');
+        $response->assertStatus(200);
+        
+        $kategori = Kategori::where('nama', 'kertasoke')->update(['nama' => 'testingTest']);
+        
+        $response = $this->actingAs($user)->get('/kategori');
+    
+    }
+
+    public function test_delete_data_kategori_page()
+    {
+        $kategori = Kategori::where('nama', 'testingTest')->delete();
+
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/kategori');
+        $response->assertStatus(302);
     }
 }
