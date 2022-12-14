69fd330913c5f3b0ba76a62c2037f73e6faa8a93
ghaan1 <gono0608@gmail.com>
2022-12-14 08:29:46 +0700

    cloud 2.4

--- resources/views/layouts/layoutAdmin.blade.php
+++ resources/views/layouts/layoutAdmin.blade.php
@@ -29,7 +29,8 @@ Coded by www.creative-tim.com
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
   <!-- CSS Files -->
-  <link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+  {{-- <link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" /> --}}
+  <link rel='stylesheet' href='https://ghani-csisw3i77q-uc.a.run.app/assets/css/bootstrap.min.css'>
   <link href="{{ URL::asset('assets/css/paper-dashboard.css') }}?v=2.0.1" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="{{ URL::asset('assets/demo/demo.css') }}" rel="stylesheet" />
